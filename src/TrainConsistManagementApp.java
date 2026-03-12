e2a65e79e3247a2dfda7f91537670e0052219d6f
sindhurmarella1835@gmail.com <sindhurmarella123@gmail.com>
2026-03-12 10:21:04 +0530

    Coded UC9 - Grouping bogies by type using groupingBy and Strams API

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,32 +1,30 @@
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.Comparator;
-
+import java.util.Map;
 
 public class TrainConsistManagementApp {
 	/*
-	 * MAIN CLASS - UseCase8TrainManagementApp
+	 * MAIN CLASS - UseCase9TrainManagementApp
 	 *
  *
- * Use Case 8: Filter Passenger Bogies Using Streams
+ * Use Case 9: Group bogies by Type
  *
  * Description:
- * This class filters passenger bogies based on seating
- * capacities using Stram API
+ * This class groups similar bogies together using
+ * Java Stream Collectors.groupingBy()
  *
  * At this stage, the application:
  * - Creates a list of bogies
- * - Converts list into stream
- * - Apllies a filter logic
- * - Collects filtered result
- * - Displays qualifying size
+ * - Streams the list
+ * - Groups bogie by name
+ * - Stores grouped data in a Map
+ * - Displays grouping structure
  *
- * This maps functional filtering using Strams
+ * This maps classification logic using groupingBy.
  *
  * @author Developer
- * @version 8.0
+ * @version 9.0
  */
 
 	static class Bogie{
@@ -42,32 +40,39 @@ public class TrainConsistManagementApp {
 	public static void main(String[] args) {
 		
 		// Display welcome message
-		System.out.println("I===================================================I");
-		System.out.println("||   UC8 - Filter Passenger Bogies using Streams   ||");
-		System.out.println("I===================================================I\n");
+		System.out.println("I==================================I");
+		System.out.println("||   UC9 - Group Bogies by Type   ||");
+		System.out.println("I==================================I\n");
 		
 		
 		List<Bogie> trainConsist = new ArrayList<>();
 		
 		// Add Bogies and Capacities into the HashMap
-		trainConsist.add(new Bogie("Sleeper",300));
-		trainConsist.add(new Bogie("AC", 80));
-		trainConsist.add(new Bogie("First class", 60));
+		trainConsist.add(new Bogie("Sleeper",72));
+		trainConsist.add(new Bogie("AC", 64));
+		trainConsist.add(new Bogie("First class", 48));
+		trainConsist.add(new Bogie("AC", 64));
+		trainConsist.add(new Bogie("Sleeper",70));
 		trainConsist.add(new Bogie("General", 150));
+		trainConsist.add(new Bogie("AC", 60));
+		trainConsist.add(new Bogie("Sleeper",74));
+		trainConsist.add(new Bogie("First class", 50));
 
-		System.out.println("Before sorting: \n");
+		System.out.println("Before grouping: \n");
 		for(Bogie bogie : trainConsist) {
 			System.out.println(bogie.name + " -> " + bogie.capacity);
 		}
 		
-		List<Bogie> filteredBogies = trainConsist.stream()
-					.filter(b -> b.capacity > 100)
-					.collect(Collectors.toList());
+		Map<String,List<Bogie>> groupedBogies = trainConsist.stream()
+					.collect(Collectors.groupingBy(bogie -> bogie.name));
 		
-		System.out.println("\nFiltered Bogies: (Capacity > 100) \n");
-		for(Bogie bogie : filteredBogies) {
-			System.out.println(bogie.name + " -> " + bogie.capacity);
+		System.out.println("\nGrouped Bogies: \n");
+		for(Map.Entry<String, List<Bogie>> bogieMap : groupedBogies.entrySet()) {
+			System.out.println(bogieMap.getKey());
+			for(Bogie b :bogieMap.getValue()) {
+				System.out.println("Capacity - " + b.capacity);
+			}
+			System.out.println();
 		}
-
 	}
 }
