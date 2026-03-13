4204dce975791a1047d18f677a4e4a92e624ad2e
sindhurmarella1835@gmail.com <sindhurmarella123@gmail.com>
2026-03-13 22:51:27 +0530

    Coded UC20 - Exception Handling During Searc Operations

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,30 +1,46 @@
 
 import java.util.*;
 import java.util.stream.*;
-import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /*
- * Use Case 18: Linear Search for Bogie ID
+ * Use Case 20: Exception Handling during Search Operations
  *
  * Description:
- * This class demonstrates searching for a specific bogie ID using a simple Linear Search algorithm
+ * This class prevents searching when no bogies
+ * exist by applying fail-fast validarion
+ * using exceptions
  *
  * At this stage, the application:
- * - Creates an array of bogie IDs
- * - Accepts a search key
- * - Traverses array sequentially
- * - Stops when match is found
- * - Displays search result
+ * - Creates bogie collection
+ * - Validates system state
+ * - Throws exception if empty
+ * - Stops invalid search operation
+ * - Displays meaningful message
  *
- * This maps basic search logic using sequential travel
+ * This maps defendive programmingusing runtime exception
  *
  * @author Developer
- * @version 18.0
+ * @version 20.0
  */
 
 public class TrainConsistManagementApp {
+	
+	static class Bogie { 
+		String name;
+		int capacity;
+
+		Bogie(String name, int capacity) {
+			this.name = name;
+			this.capacity = capacity;
+		}
+
+		@Override
+		public String toString() {
+			return name + " -> " + capacity;
+		}
+	}
 	
 	public static void main(String[] args) {
 		// Display welcome banner
@@ -33,38 +49,39 @@ public class TrainConsistManagementApp {
 		System.out.println("==========================================\n");
 
 		System.out.println("================================================");
-		System.out.println("   UC19 - Binary Search for Bogie ID   ");
+		System.out.println("   UC20 - Exception Handling During Search   ");
 		System.out.println("================================================\n");
 
 		// Initialize an array of passenger bogie capacities
-		String[] bogieIds = {"B101", "B102", "B103", "B104", "B105"};
-		// Available Bogies
+		List<String> trainConsist = new ArrayList<>();
+		trainConsist.add("B101");
+		trainConsist.add("B102");
+		trainConsist.add("B103");
+		trainConsist.add("B104");
+		trainConsist.add("B105");
+		
+		if(trainConsist.size() == 0) throw new IllegalStateException("Train has no bogies");
+				
+		// Before searching
 		System.out.println("Available Bogie IDs: ");
 		System.out.print("[ ");
-		for(int i = 0; i < bogieIds.length - 1; i++) {
-			System.out.print(bogieIds[i] + ", ");
+		for(int i = 0; i < trainConsist.size() - 1; i++) {
+			System.out.print(trainConsist.get(i) + ", ");
 		}
-		System.out.println(bogieIds[bogieIds.length - 1] + " ]");
+		System.out.println(trainConsist.get(trainConsist.size() - 1) + " ]");
 		// Required Bogie ID
-		String requiredBogieID = "B104";
+		String requiredBogieID = "B103";
 		//String requiredBogieID = "B109";
 		// Search logic
 		boolean found = false;
-		int start = 0;
-		int end = bogieIds.length - 1;
-		while(start <= end) {
-			int mid = start + (end - start) / 2;
-			if(bogieIds[mid].equals(requiredBogieID)) {
+		for(String s : trainConsist) {
+			if(requiredBogieID.equals(s)) {
 				found = true;
 				break;
 			}
-			else if(bogieIds[mid].compareTo(requiredBogieID) < 0) {
-				start = mid + 1;
-			}
-			else end = mid - 1;
 		}
 		
 		System.out.println(found ? "Bogie " + requiredBogieID + " FOUND in train consist." : "Bogie " + requiredBogieID + " NOT FOUND in train consist.");
-		System.out.println("UC18 search completed...");
+		System.out.println("UC20 search with safety completed...");
 	}
 }
